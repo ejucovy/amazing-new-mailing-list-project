0946a1a6e1225f1123036451157fc61df2546807
Ethan Jucovy <ejucovy@gmail.com>
2011-12-26 09:24:49 -0500

    accept/decline invitation

--- opencore/members/urls.py
+++ opencore/members/urls.py
@@ -11,6 +11,9 @@ urlpatterns = patterns(
 
     url('^(?P<username>[\w\d-]+)/$', 'member_home', name='member_home'),
     url('^(?P<username>[\w\d-]+)/account/$', 'member_account', name='member_account'),
+
+    url('^(?P<username>[\w\d-]+)/account/project-invites/(?P<project_slug>[\w\d-]+)/$', 'member_project_invites', name='member_project_invites'),
+
     url('^(?P<username>[\w\d-]+)/profile/$', 'member_profile', name='member_profile'),
     url('^(?P<username>[\w\d-]+)/profile-edit/$', 'member_profile_edit', name='member_profile_edit'),
 
--- opencore/members/views.py
+++ opencore/members/views.py
@@ -15,8 +15,46 @@ def index_of_members(request):
 def member_home(request, username):
     pass
 
+@allow_http("GET")
+@rendered_with("opencore/member/account.html")
 def member_account(request, username):
-    pass
+    user = User.objects.get(username=username)
+    if user != request.user:
+        return HttpResponseForbidden()
+    invites = ProjectInvite.objects.filter(user=user)
+    return locals()
+
+@allow_http("POST")
+def member_project_invites(request, username, project_slug):
+    user = User.objects.get(username=username)
+    project = Project.objects.get(slug=project_slug)
+    if user != request.user:
+        return HttpResponseForbidden()
+
+    invites = ProjectInvite.objects.filter(
+        user=user, project=project)
+    if len(invites) == 0:
+        messages.error(
+            request, "No such invitation exists.")
+        return redirect("member_account", [user.username])
+
+    invites.delete()
+    if request.POST['action'] == 'reject':
+        return redirect("member_account", [user.username])
+    
+    assert request.POST['action'] == 'accept'
+    if ProjectMember.objects.filter(
+        user=user, project=project).count() > 0:
+        messages.info(
+            request, 
+            "You are already a member of this project.")
+        return redirect("member_account", [user.username])
+        
+    membership = ProjectMember(user=user, project=project,
+                               role="ProjectMembeR")
+    membership.save()
+    messages.success(request, "Poof!  You're a member.")
+    return redirect("member_account", [user.username])
 
 def member_profile(request, username):
     pass
